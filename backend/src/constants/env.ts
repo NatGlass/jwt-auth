a9c2064770b3b99de6232d0961ffccad4c91e83f
Visibyte <nathanglass26@gmail.com>
2024-08-02 18:49:50 +0100

    env

---
+++ backend/src/constants/env.ts
@@ -0,0 +1,12 @@
+const getEnv = (key: string, defaultValue?: string): string => {
+  const value = process.env[key] || defaultValue;
+
+  if (value === undefined) {
+    throw new Error(`Missing env variable ${key}`);
+  }
+
+  return value;
+};
+
+export const MONGO_URI = getEnv("MONGO_URI");
+export const PORT = getEnv("PORT", "4000");
